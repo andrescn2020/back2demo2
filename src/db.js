b5766acb4e5af0ad4934052e862830c0893b051e
andrescn2020 <andrescn_2010@hotmail.com>
2023-08-04 18:23:03 -0300

    Update db.js

--- src/db.js
+++ src/db.js
@@ -3,6 +3,48 @@ const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
 const {
+  DB_HOST,
+  DB_USER,
+  DB_PASSWORD,
+  DB_NAME
+} = process.env;
+
+const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+  host: DB_HOST,
+  dialect: 'postgres', // Cambia a 'mysql', 'sqlite', 'postgres' u otro si estás usando una base de datos diferente
+  logging: false,   // Establece en console.log para ver las consultas SQL en la consola
+});
+
+const basename = path.basename(__filename);
+
+const modelDefiners = [];
+
+fs.readdirSync(path.join(__dirname, '/models'))
+  .filter((file) => (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js'))
+  .forEach((file) => {
+    modelDefiners.push(require(path.join(__dirname, '/models', file)));
+  });
+
+modelDefiners.forEach(model => model(sequelize));
+let entries = Object.entries(sequelize.models);
+let capsEntries = entries.map((entry) => [entry[0][0].toUpperCase() + entry[0].slice(1), entry[1]]);
+sequelize.models = Object.fromEntries(capsEntries);
+
+const { User, Publication } = sequelize.models;
+
+Publication.belongsToMany(User, { through: "Publication_User" });
+User.belongsToMany(Publication, { through: "Publication_User" });
+
+module.exports = {
+  ...sequelize.models,
+  conn: sequelize,
+};
+
+/*require('dotenv').config();
+const { Sequelize } = require('sequelize');
+const fs = require('fs');
+const path = require('path');
+const {
   DB_DEPLOY
 } = process.env;
 
@@ -55,3 +97,4 @@ module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
   conn: sequelize,     // para importar la conexión { conn } = require('./db.js');
 };
+*/
